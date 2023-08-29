6f4dcd6e68afd90b3684a98922bd4126ae31b050
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-08-29 19:35:02 +0530

    added from_dict method in User class and implemented PUT updateuser endpoint

--- entities/user.py
+++ entities/user.py
@@ -13,9 +13,20 @@ class User:
         }
 
     @staticmethod
+    def from_dict(dict_obj):
+        return User(dict_obj['id'], dict_obj['name'])
+
+    @staticmethod
     def add_user(name):
         _id = len(users) + 1
         user = User(_id, name)
         users.append(user.to_dict())
+
+
+
+
+
+
+
 
 
