9cb58ae6c88cbec08d1877ec1adb7910aaf813d6
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-08-31 11:05:37 +0530

    adding capability to pick api data from data.json for mocking purpose

--- entities/mockdata.py
+++ entities/mockdata.py
@@ -1,4 +1,10 @@
+import json
+
 # Mock data
 users = []
 posts = []
 comments = []
+
+with open('entities/data.json', 'r') as json_file:
+    data = json.load(json_file)
+    users, posts, comments = data['users'], data['posts'], data['comments']
